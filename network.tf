c27e342d8749d75abe9e32b63f120a2deb5915b7
keyproco <keddarts@gmail.com>
2020-09-12 10:22:40 +0300

    Deploying multi-region vpc peering

--- network.tf
+++ network.tf
@@ -64,3 +64,88 @@ resource "aws_subnet" "subnet_1_london" {
   cidr_block = "192.168.1.0/24"
 }
 
+
+
+
+# Peering connection request from eu-west-3
+
+resource "aws_vpc_peering_connection" "euwest3-to-euwest2" {
+  provider    = aws.region-master
+  peer_vpc_id = aws_vpc.vpc_master_london.id
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_region = var.region-worker
+}
+
+# Accept VPC peering request in eu-west-3 from eu-west-3
+
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.euwest3-to-euwest2.id
+  auto_accept               = true
+}
+
+
+
+# Create Routing table in eu-west-3
+
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  route {
+    cidr_block                = "192.168.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.euwest3-to-euwest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+
+
+
+# Overwrite default route table of VPC master with our route table entries
+
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+
+
+# Create Routing table in eu-west-3
+
+resource "aws_route_table" "internet_route_london" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_master_london.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-london.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.euwest3-to-euwest2.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+# Overwrite default route table of VPC worker with our route table entries
+
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region-worker
+  vpc_id         = aws_vpc.vpc_master_london.id
+  route_table_id = aws_route_table.internet_route_london.id
+}
+
